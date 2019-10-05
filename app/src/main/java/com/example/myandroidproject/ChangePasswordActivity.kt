bcbb9775b6515ce7e9715c540b9e1ad8e44825da
Dmitry Belyakov <osnovdmitry@mail.ru>
2019-10-05 22:42:56 +0300

    home 3 add

---
+++ app/src/main/java/com/example/myandroidproject/ChangePasswordActivity.kt
@@ -0,0 +1,53 @@
+package com.example.myandroidproject
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_change_password.*
+
+class ChangePasswordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_password)
+
+        val backButton:Button = findViewById(R.id.backButton)
+
+        backButton.setOnClickListener {
+            super.onBackPressed()
+        }
+
+        emailTextView.text = "Логин: ${intent.getStringExtra("email")}"
+
+        okButton.setOnClickListener {
+            if(et_sign_in_changePass.text.toString() == double_et_sign_in_changePass.text.toString()){
+                LoginActivity.passwordRepository.password = et_sign_in_changePass.text.toString()
+                super.onBackPressed()
+            }
+            else {
+                setPasswordError()
+            }
+
+        }
+
+        double_et_sign_in_changePass.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                double_ti_sign_in_changePass.error = null
+                double_ti_sign_in_changePass.isErrorEnabled = false
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+        })
+    }
+
+    private fun setPasswordError() {
+        double_ti_sign_in_changePass.error = getString(R.string.incorrect_password)
+    }
+}
---
+++ app/src/main/java/com/example/myandroidproject/LoginActivity.kt
@@ -0,0 +1,80 @@
+package com.example.myandroidproject
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val imageView: ImageView = findViewById(R.id.imageView2)
+        imageView.setImageResource(R.drawable.img)
+
+        val changePasswordButton: Button = findViewById(R.id.changePasswordButton)
+        changePasswordButton.setOnClickListener {
+            val intent = Intent(this, ChangePasswordActivity::class.java)
+            intent.putExtra("email", et_sign_in_login.text.toString())
+            startActivity(intent)
+
+        }
+
+        val loginButton: Button = findViewById(R.id.loginButton)
+        loginButton.setOnClickListener {
+            if (et_sign_in_pass.text.toString() == passwordRepository.password) {
+                progressBar.visibility = View.VISIBLE
+//                var b = 0
+//                while (b != 567){
+//                    b = (1..10000).random()
+//                }
+                //progressBar.visibility = View.INVISIBLE
+                Toast.makeText(this, "Successful", Toast.LENGTH_LONG).show()
+                progressBar.visibility = View.INVISIBLE
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                progressBar.visibility = View.VISIBLE
+//                var b = 0
+//                while (b != 543){
+//                    b = (1..10000).random()
+//                }
+                Toast.makeText(this, "Wrong password", Toast.LENGTH_LONG).show()
+                setPasswordError()
+                progressBar.visibility = View.INVISIBLE
+            }
+        }
+
+        et_sign_in_pass.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                ti_sign_in_pass.error = null
+                ti_sign_in_pass.isErrorEnabled = false
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+        })
+
+
+    }
+
+    object passwordRepository {
+        var password: String = "123456"
+    }
+
+    private fun setPasswordError() {
+        ti_sign_in_pass.error = getString(R.string.wrong_password)
+    }
+}
